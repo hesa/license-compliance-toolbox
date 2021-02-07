ae61271001b4f2a26ffa476a360260572736e1c0
Maximilian Huber <gh@maxhbr.de>
2021-02-07 21:09:49 +0100

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   ort = pkgs.callPackage ./oss-review-toolkit-ort {};
--- flake.nix
+++ flake.nix
@@ -7,11 +7,9 @@
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux = import ./default.nix {
-      system = "x86_64-linux";
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+
     };
-
-
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.ort;
-
   };
 }
